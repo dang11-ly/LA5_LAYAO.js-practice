203267d2dadbca99c3b0ba4b97002dcd70dc678b
dang11-ly <layaoamandayvelle@gmail.com>
2024-09-23 17:52:55 +0800

    Add files via upload

---
+++ strings.js.js
@@ -0,0 +1,14 @@
+// Strings
+var favActorFirstName = "William Bradley";
+var favActorLastName = "Pitt";
+var fullName = favActorFirstName + " " + favActorLastName;
+var uppercase = fullName.toUpperCase();
+var message = "My favorite Actor is " + uppercase + ". He is best actor hollywood movie.";
+
+message += " His best show is Sillicon Valley.";
+
+console.log("favActorFirstName:", favActorFirstName);
+console.log("favActorLastName:", favActorLastName);
+console.log("fullName:", fullName);
+console.log("uppercase:", uppercase);
+console.log("message:", message);
